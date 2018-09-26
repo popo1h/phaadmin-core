dc2e3179823bfda2483184ef8ea8d997da558c20
popo1h <popo1h@sina.com>
2018-09-26 20:16:23 +0800

    auto auth append prefix

--- src/ActionAuth/AutoByActionActionAuth.php
+++ src/ActionAuth/AutoByActionActionAuth.php
@@ -12,19 +12,27 @@ class AutoByActionActionAuth
      * @var Action
      */
     private $action;
+    /**
+     * @var string
+     */
+    private $prefix;
 
     /**
      * AutoByActionActionAuth constructor.
      * @param Action $action
+     * @param string $prefix
      */
-    public function __construct(Action $action)
+    public function __construct(Action $action, $prefix = '')
     {
         $this->action = $action;
+        if ($prefix) {
+            $this->prefix = $prefix . '-';
+        }
     }
 
     public function getName()
     {
-        return ActionAuth::AUTH_NAME_AUTO_BY_ACTION_PREFIX . forward_static_call_array([$this->action, 'getName'], []);
+        return ActionAuth::AUTH_NAME_AUTO_BY_ACTION_PREFIX . $this->prefix . forward_static_call_array([$this->action, 'getName'], []);
     }
 
     public function getTitle()
